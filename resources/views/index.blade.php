b25234d7dbd46dac4fbad572cf87dc7aff21d5ba
Mishu Rahman <mishu.cse616@gmail.com>
2022-06-23 18:25:05 +0600

    modification of database config and filter part

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
         <div class="w-2/3 relative">
             <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white placeholder-gray-700 border-none px-4 py-2 pl-8 text-xs">
             <div class="absolute top-0 flex items-center h-full ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                   </svg>
             </div>
